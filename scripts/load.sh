ad876a95297e9b48687ab5a7cd302c5601e1e4fe
David Ishmael <dishmael@opsvision.com>
2017-01-24 20:11:47 +0000

    Corrected task path error

--- scripts/load.sh
+++ scripts/load.sh
@@ -3,11 +3,13 @@
 # This is a helper script for loading our plugin to expediate testing during development
 
 PLUGIN='snap-plugin-publisher-awssqs'
-TASK='~/tasks/awssqs.yaml'
+TASK=${HOME}'/tasks/awssqs.yaml'
 
 if [ ! -d ${GOPATH} ]; then echo "GOPATH may not be set correctly"; exit -1; fi
 
 if [ -e ${GOPATH}/bin/${PLUGIN} ]; then rm ${GOPATH}/bin/${PLUGIN}; fi
+
+if [ ! -e ${TASK} ]; then echo "Task file does not exist"; exit -1; fi
 
 go install && \
   snaptel plugin load ~/golang/bin/${PLUGIN} && \
